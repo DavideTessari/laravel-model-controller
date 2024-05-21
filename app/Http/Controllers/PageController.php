f171bdc989d565ab7332f2abf1e2714f861d2e8d
DavideTessari <davtex@gmail.com>
2024-05-21 18:50:01 +0200

    fix

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index() {
         $movies = Movie::all();
-    
-        return view('home', $data);
+
+        return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                   <h5 class="card-title">Card title: {{ $movie->title }}</h5>
-                  <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{ $movie->original_title}}</h6>
+                  <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{ $movie->original_title }}</h6>
                   <p class="card-text">Nationality: {{ $movie->nationality }}</p>
                   <p class="card-text">Date: {{ $movie->date }}</p>
                   <p class="card-text">Vote: {{ $movie->vote }}</p>
